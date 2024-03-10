e66d04a3b276ef05a82997c8c6ccdd7122834bb1
nwcnai <joopedropereiramilani15@gmail.com>
2024-03-09 23:31:00 -0300

    primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+function redirecionarParaSaibaMais() {
+    var urlSaibaMais = document.querySelector('#basquete').href;
+    window.location.href = urlSaibaMais;
+}
+
+function redirecionarParaSaibaMais2() {
+    var urlSaibaMais = document.querySelector('#voleiareia').href;
+    window.location.href = urlSaibaMais;
+}
+
+function redirecionarParaSaibaMais3() {
+    var urlSaibaMais = document.querySelector('#voleiequipe').href;
+    window.location.href = urlSaibaMais;
+}
+
+function redirecionarParaLoja() {
+    var urlSaibaMais = document.querySelector('#lojacap').href;
+    window.location.href = urlSaibaMais;
+}
+
+function redirecionarCamisa2024I() {
+    var urlSaibaMais = document.querySelector('#camisa2024I').href;
+    window.location.href = urlSaibaMais;
+}
+
+function redirecionarCamisa2024II() {
+    var urlSaibaMais = document.querySelector('#camisa2024II').href;
+    window.location.href = urlSaibaMais;
+}
+
+function redirecionarCamisa2018() {
+    var urlSaibaMais = document.querySelector('#camisa2018').href;
+    window.location.href = urlSaibaMais;
+}
+
+function redirecionarCamisa2017() {
+    var urlSaibaMais = document.querySelector('#camisa2017').href;
+    window.location.href = urlSaibaMais;
+}
+
+function redirecionarCamisa2016() {
+    var urlSaibaMais = document.querySelector('#camisa2016').href;
+    window.location.href = urlSaibaMais;
+}
+
+function redirecionarCamisa2014() {
+    var urlSaibaMais = document.querySelector('#camisa2014').href;
+    window.location.href = urlSaibaMais;
+}
+
+function redirecionarCamisa2013() {
+    var urlSaibaMais = document.querySelector('#camisa2013').href;
+    window.location.href = urlSaibaMais;
+}
